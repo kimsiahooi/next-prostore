e91f7396208c1eb6d7082e56225e78755018629c
Ooi Kim Siah <kimsiahooi@gmail.com>
2026-01-25 12:45:46 +0800

    14

--- components/shared/header/header.tsx
+++ components/shared/header/header.tsx
@@ -1,9 +1,7 @@
-import { ShoppingCart, UserIcon } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
-import { Button } from "@/components/ui/button";
 import { APP_NAME } from "@/lib/contants";
-import { ModeToggle } from "./mode-toggle";
+import Menu from "./menu";
 
 export default function Header() {
   return (
@@ -24,20 +22,7 @@ export default function Header() {
           </Link>
         </div>
 
-        <div className="space-x-2">
-          <ModeToggle />
-          <Button asChild variant="ghost">
-            <Link href="/cart">
-              <ShoppingCart /> Cart
-            </Link>
-          </Button>
-
-          <Button asChild>
-            <Link href="/sign-in">
-              <UserIcon /> Sign In
-            </Link>
-          </Button>
-        </div>
+        <Menu />
       </div>
     </header>
   );
