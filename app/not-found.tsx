c3feb1fbcd2fe6faee6a8bc68f21c16eddd3e1f9
Ooi Kim Siah <kimsiahooi@gmail.com>
2026-01-25 12:34:15 +0800

    13

---
+++ app/not-found.tsx
@@ -0,0 +1,26 @@
+"use client";
+import Image from "next/image";
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
+import { APP_NAME } from "@/lib/contants";
+
+export default function NotFoundPage() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen">
+      <Image
+        src="/images/logo.svg"
+        width={48}
+        height={48}
+        alt={`${APP_NAME} logo`}
+        priority
+      />
+      <div className="p-6 rounded-lg shadow-md text-center">
+        <h1 className="text-3xl font-bold mb-4">Not Found</h1>
+        <p className="text-destructive">Could not find requested page</p>
+        <Button variant="outline" className="mt-4 ml-2" asChild>
+          <Link href="/">Home</Link>
+        </Button>
+      </div>
+    </div>
+  );
+}
