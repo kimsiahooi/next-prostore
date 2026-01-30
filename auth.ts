f5d7aea32bdea38dfcf5dfcf04daf1cd71ea5e01
ksooi.9dm <ks.ooi@9dm.asia>
2026-01-31 06:31:14 +0800

    32

---
+++ auth.ts
@@ -0,0 +1,65 @@
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { compareSync } from "bcrypt-ts-edge";
+import NextAuth from "next-auth";
+import Credentials from "next-auth/providers/credentials";
+import { prisma } from "@/lib/prisma";
+
+export const { handlers, signIn, signOut, auth } = NextAuth({
+  pages: {
+    signIn: "/sign-in",
+    error: "/sign-in",
+  },
+  session: {
+    strategy: "jwt",
+    maxAge: 30 * 24 * 60 * 60,
+  },
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    Credentials({
+      credentials: {
+        email: { type: "email" },
+        password: { type: "password" },
+      },
+      async authorize(credentials) {
+        if (!credentials) return null;
+
+        const user = await prisma.user.findFirst({
+          where: {
+            email: credentials.email as string,
+          },
+        });
+
+        if (user?.password) {
+          const isMatch = compareSync(
+            credentials.password as string,
+            user.password,
+          );
+
+          if (isMatch) {
+            return {
+              id: user.id,
+              name: user.name,
+              email: user.email,
+              role: user.role,
+            };
+          }
+        }
+
+        return null;
+      },
+    }),
+  ],
+  callbacks: {
+    async session({ session, user, trigger, token }) {
+      if (token.sub) {
+        session.user.id = token.sub;
+      }
+
+      if (trigger === "update") {
+        session.user.name = user.name;
+      }
+
+      return session;
+    },
+  },
+});
--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,5 +1,5 @@
 import { PrismaMariaDb } from "@prisma/adapter-mariadb";
-import { PrismaClient } from "../app/generated/prisma/client";
+import { PrismaClient } from "@/app/generated/prisma/client";
 
 const adapter = new PrismaMariaDb({
   host: process.env.DATABASE_HOST,
