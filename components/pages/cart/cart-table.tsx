b0918dc6c01652f73bcc37d22cb890dbbc3e02f1
Ooi Kim Siah <kimsiahooi@gmail.com>
2026-02-03 07:43:12 +0800

    56

--- components/pages/cart/cart-table.tsx
+++ components/pages/cart/cart-table.tsx
@@ -1,9 +1,25 @@
 "use client";
 
+import { Loader, Minus, Plus } from "lucide-react";
+import Image from "next/image";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
 import { useTransition } from "react";
-import type { getMyCart } from "@/lib/actions/cart.actions";
+import { toast } from "sonner";
+import { Button } from "@/components/ui/button";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import {
+  addItemToCart,
+  type getMyCart,
+  removeItemFromCart,
+} from "@/lib/actions/cart.actions";
 
 export default function CartTable({
   cart,
@@ -22,8 +38,79 @@ export default function CartTable({
         </div>
       ) : (
         <div className="grid md:grid-cols-4 md:gap-5">
-          <div>
-            <div className="overflow-x-auto md:col-span-3">Table</div>
+          <div className="overflow-x-auto md:col-span-3">
+            <Table>
+              <TableHeader>
+                <TableRow>
+                  <TableHead>Item</TableHead>
+                  <TableHead className="text-center">Quantity</TableHead>
+                  <TableHead className="text-right">Price</TableHead>
+                </TableRow>
+              </TableHeader>
+              <TableBody>
+                {cart.items.map((item) => (
+                  <TableRow key={item.slug}>
+                    <TableCell>
+                      <Link
+                        href={`/product/${item.slug}`}
+                        className="flex items-center">
+                        <Image
+                          src={item.image}
+                          alt={item.name}
+                          width={50}
+                          height={50}
+                        />
+                        <span className="px-2">{item.name}</span>
+                      </Link>
+                    </TableCell>
+                    <TableCell className="flex-center gap-2">
+                      <Button
+                        disabled={isPending}
+                        variant="outline"
+                        type="button"
+                        onClick={() =>
+                          startTransition(async () => {
+                            const res = await removeItemFromCart(
+                              item.productId,
+                            );
+
+                            if (!res.success) {
+                              toast.error(res.message);
+                            }
+                          })
+                        }>
+                        {isPending ? (
+                          <Loader className="size-4 animate-spin" />
+                        ) : (
+                          <Minus className="size-4" />
+                        )}
+                      </Button>
+                      <span>{item.qty}</span>
+                      <Button
+                        disabled={isPending}
+                        variant="outline"
+                        type="button"
+                        onClick={() =>
+                          startTransition(async () => {
+                            const res = await addItemToCart(item);
+
+                            if (!res.success) {
+                              toast.error(res.message);
+                            }
+                          })
+                        }>
+                        {isPending ? (
+                          <Loader className="size-4 animate-spin" />
+                        ) : (
+                          <Plus className="size-4" />
+                        )}
+                      </Button>
+                    </TableCell>
+                    <TableCell className="text-right">${item.price}</TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
           </div>
         </div>
       )}
