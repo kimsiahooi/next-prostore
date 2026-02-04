dd1b9da1757f76346044ee1e98ee3fd539abe10a
ksooi.9dm <ks.ooi@9dm.asia>
2026-02-04 08:57:01 +0800

    63

--- auth.ts
+++ auth.ts
@@ -107,6 +107,20 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
       return token;
     },
     authorized({ request }) {
+      const protectedPaths = [
+        /\/shipping-address/,
+        /\/payment-method/,
+        /\/place-order/,
+        /\/profile/,
+        /\/user\/(.*)/,
+        /\/order\/(.*)/,
+        /\/admin/,
+      ];
+
+      const { pathname } = request.nextUrl;
+
+      if (!auth && protectedPaths.some((p) => p.test(pathname))) return false;
+
       if (!request.cookies.get("sessionCartId")) {
         const sessionCartId = crypto.randomUUID();
 
